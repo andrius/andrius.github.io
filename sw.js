b55cfccd2d7a4022c8f6e2ac4fb6edb33330a773
andrius <andrius.kairiukstis@gmail.com>
2024-01-05 20:41:20 +0000

    Deploying to master from @ andrius/blog@7e6758eb40470d11d889cf3d54b334daa9a31150 🚀

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-var CACHE_NAME = "pixyll2-20200620084841";
+var CACHE_NAME = "pixyll2";
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202006200848",
+        "/css/pixyll.css?202401052041",
         "/"
       ]);
     })
