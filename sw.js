ac8b60f5ae9d010cce48af8a1aa7aebfaaead693
andrius <andrius.kairiukstis@gmail.com>
2025-05-14 18:35:10 +0000

    Deploying to master from @ andrius/blog@dc008278aaa9ae7fda9e0d4ad9bac78522b6a4f1 🚀

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202401052053",
+        "/css/pixyll.css?202505141835",
         "/"
       ]);
     })
